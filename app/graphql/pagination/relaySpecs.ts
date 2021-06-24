f39d12754feb4aef8791fec79233d5b72d828c20
MariethPerpi <mperpinan@vhmsoluciones.com>
2021-06-24 18:57:47 -0500

    Ajuste de libreria de paginacion

--- app/graphql/pagination/relaySpecs.ts
+++ app/graphql/pagination/relaySpecs.ts
@@ -3,22 +3,22 @@ import { ArgsType, ClassType, Field, ObjectType } from 'type-graphql';
 
 @ArgsType()
 export class ConnectionArgs implements Relay.ConnectionArguments {
-  @Field(type => String, {
+  @Field((type) => String, {
     nullable: true,
-    description: 'Paginate before opaque cursor'
+    description: 'Paginate before opaque cursor',
   })
   before?: Relay.ConnectionCursor;
 
-  @Field(type => String, {
+  @Field((type) => String, {
     nullable: true,
-    description: 'Paginate after opaque cursor'
+    description: 'Paginate after opaque cursor',
   })
   after?: Relay.ConnectionCursor;
 
-  @Field(type => Number, { nullable: true, description: 'Paginate first' })
+  @Field((type) => Number, { nullable: true, description: 'Paginate first' })
   first?: number;
 
-  @Field(type => Number, { nullable: true, description: 'Paginate last' })
+  @Field((type) => Number, { nullable: true, description: 'Paginate last' })
   last?: number;
 }
 
@@ -30,13 +30,13 @@ export function ConnectionType<
 >(nodeName: string, edgeClass: ClassType<EdgeType>) {
   @ObjectType(`${nodeName}Connection`, { isAbstract: true })
   abstract class Connection implements Relay.Connection<NodeType> {
-    @Field(type => PageInfo)
+    @Field((type) => PageInfo)
     pageInfo!: PageInfo;
 
-    @Field(type => [edgeClass])
+    @Field((type) => [edgeClass])
     edges!: EdgeType[];
 
-    @Field(type => Number, { nullable: true, description: 'Total Count' })
+    @Field((type) => Number, { nullable: true, description: 'Total Count' })
     totalCount?: number;
   }
 
@@ -46,11 +46,11 @@ export function ConnectionType<
 export function EdgeType<NodeType>(nodeName: string, nodeType: ClassType<NodeType>) {
   @ObjectType(`${nodeName}Edge`, { isAbstract: true })
   abstract class Edge implements Relay.Edge<NodeType> {
-    @Field(type => nodeType)
+    @Field((type) => nodeType)
     node!: NodeType;
 
-    @Field(type => String, {
-      description: 'Used in `before` and `after` args'
+    @Field((type) => String, {
+      description: 'Used in `before` and `after` args',
     })
     cursor!: Relay.ConnectionCursor;
   }
@@ -60,15 +60,15 @@ export function EdgeType<NodeType>(nodeName: string, nodeType: ClassType<NodeTyp
 
 @ObjectType()
 class PageInfo implements Relay.PageInfo {
-  @Field(type => Boolean)
-  hasNextPage?: boolean | null | undefined;
+  @Field((type) => Boolean)
+  hasNextPage!: boolean;
 
-  @Field(type => Boolean)
-  hasPreviousPage?: boolean | null | undefined;
+  @Field((type) => Boolean)
+  hasPreviousPage!: boolean;
 
-  @Field(type => String, { nullable: true })
-  startCursor?: string | null | undefined;
+  @Field((type) => String, { nullable: true })
+  startCursor!: string | null;
 
-  @Field(type => String, { nullable: true })
-  endCursor?: string | null | undefined;
+  @Field((type) => String, { nullable: true })
+  endCursor!: string | null;
 }
