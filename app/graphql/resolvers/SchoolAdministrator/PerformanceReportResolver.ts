542df28c8670ac613ef53f4a28ac5cca037f0f6c
MasterVHM <scrummaster@vhmsoluciones.com>
2022-08-09 21:51:49 -0500

    update

--- app/graphql/resolvers/SchoolAdministrator/PerformanceReportResolver.ts
+++ app/graphql/resolvers/SchoolAdministrator/PerformanceReportResolver.ts
@@ -32,4 +32,46 @@ export class PerformanceReportResolver {
     return true;
   }
 
+  @Mutation(() => Boolean)
+  async generatePerformanceLevelExample2(
+    @Arg('id', () => String) id: string,
+    @Ctx() context: IContext
+  ): Promise<Boolean | null> {
+    const puppeteer = require("puppeteer");
+    const fs = require("fs-extra");
+    const hbs = require("handlebars");
+    const path = require("path");
+    const data = require('../../../reports/performanceReport/data.json');
+
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      console.log(data)
+      const content = await this.compile('index', data);
+      console.log(content)
+      await page.setContent(content);
+      await page.pdf({
+        path: 'output.pdf',
+        format: 'A4',
+        printBackground: true
+      })
+      console.log("done creating pdf");
+      await browser.close();
+      //process.exit();
+    } catch (e) {
+      console.log(e);
+    }
+    return true;
+  }
+
+  async compile(templateName: any, data: any) {
+    const path = require("path");
+    const fs = require("fs-extra");
+    const hbs = require("handlebars");
+    const filePath = path.join(process.cwd(), 'app', 'reports', 'performanceReport', `${templateName}.hbs`);
+    const html = await fs.readFile(filePath, 'utf8');
+    console.log(html)
+    return hbs.compile(html)(data);
+  };
+
 }
