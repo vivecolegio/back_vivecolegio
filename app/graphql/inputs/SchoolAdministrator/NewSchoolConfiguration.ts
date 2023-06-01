7d611fa89c0532bba34dcfd2cdae34d0f164b126
MasterVHM <scrummaster@vhmsoluciones.com>
2023-06-01 13:39:55 -0500

    update

--- app/graphql/inputs/SchoolAdministrator/NewSchoolConfiguration.ts
+++ app/graphql/inputs/SchoolAdministrator/NewSchoolConfiguration.ts
@@ -13,4 +13,9 @@ export class NewSchoolConfiguration implements Partial<SchoolConfiguration> {
   @Field({ nullable: true })
   valueString?: string;
 
+  @Field({ nullable: true })
+  schoolId?: string;
+
+  @Field({ nullable: true })
+  schoolYearId?: string;
 }
--- app/graphql/resolvers/GeneralAdministrator/SchoolResolver.ts
+++ app/graphql/resolvers/GeneralAdministrator/SchoolResolver.ts
@@ -1,5 +1,8 @@
 import { connectionFromArraySlice } from 'graphql-relay';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
 import { ObjectId } from 'mongodb';
+import ShortUniqueId from 'short-unique-id';
+import { finished } from 'stream/promises';
 import { Arg, Args, Ctx, FieldResolver, Mutation, Query, Resolver, Root } from 'type-graphql';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 
@@ -133,6 +136,86 @@ export class SchoolResolver {
     let result = await this.repository.deleteOne({ _id: new ObjectId(id) });
     return result?.result?.ok === 1 ?? true;
   }
+
+
+  @Mutation(() => Boolean)
+  async schoolLogoUploadImage(
+    @Arg('id', () => String) id: string,
+    @Arg('file', () => GraphQLUpload, { nullable: true }) file: FileUpload,
+    @Ctx() context: IContext
+  ) {
+    //console.log(context);
+    let updatedByUserId = context?.user?.authorization?.id;
+    if (file?.filename) {
+      var fs = require('fs');
+      var dir = './public/uploads/school/profile/' + id;
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      const stream = file?.createReadStream();
+      const uid = new ShortUniqueId({ length: 14 });
+      const out = fs.createWriteStream(
+        dir +
+        '/' +
+        uid() +
+        '.' +
+        file?.filename.slice(((file?.filename.lastIndexOf('.') - 1) >>> 0) + 2)
+      );
+      stream.pipe(out);
+      await finished(out);
+      let result = await this.repository.findOneBy(id);
+      result = await this.repository.save({
+        _id: new ObjectId(id),
+        ...result,
+        logo: out.path,
+        updatedByUserId,
+        version: (result?.version as number) + 1,
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async schoolImgPrincipalSignatureUploadImage(
+    @Arg('id', () => String) id: string,
+    @Arg('file', () => GraphQLUpload, { nullable: true }) file: FileUpload,
+    @Ctx() context: IContext
+  ) {
+    //console.log(context);
+    let updatedByUserId = context?.user?.authorization?.id;
+    if (file?.filename) {
+      var fs = require('fs');
+      var dir = './public/uploads/school/principalSignature/' + id;
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      const stream = file?.createReadStream();
+      const uid = new ShortUniqueId({ length: 14 });
+      const out = fs.createWriteStream(
+        dir +
+        '/' +
+        uid() +
+        '.' +
+        file?.filename.slice(((file?.filename.lastIndexOf('.') - 1) >>> 0) + 2)
+      );
+      stream.pipe(out);
+      await finished(out);
+      let result = await this.repository.findOneBy(id);
+      result = await this.repository.save({
+        _id: new ObjectId(id),
+        ...result,
+        imgPrincipalSignature: out.path,
+        updatedByUserId,
+        version: (result?.version as number) + 1,
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
   @FieldResolver((_type) => User, { nullable: true })
   async createdByUser(@Root() data: School) {
